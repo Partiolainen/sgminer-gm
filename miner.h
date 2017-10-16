62a98bbaddef3d73a1645acfeec156fa98d3a3e2
Partiolainen <petersson.kiev@gmail.com>
2017-10-16 23:28:39 +0300

    Move be32enc_vect to miner.h

--- miner.h
+++ miner.h
@@ -724,6 +724,19 @@ static inline void swab256(void *dest_p, const void *src_p)
   dest[7] = swab32(src[0]);
 }
 
+/*
+ * Encode a length len/4 vector of (uint32_t) into a length len vector of
+ * (unsigned char) in big-endian form.  Assumes len is a multiple of 4.
+ */
+static inline void
+be32enc_vect(uint32_t *dst, const uint32_t *src, uint32_t len)
+{
+	uint32_t i;
+
+	for (i = 0; i < len; i++)
+		dst[i] = htobe32(src[i]);
+}
+
 static inline void flip32(void *dest_p, const void *src_p)
 {
   uint32_t *dest = (uint32_t *)dest_p;
